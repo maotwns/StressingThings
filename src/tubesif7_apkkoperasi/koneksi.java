25194b8de9596a41e29df76d83588bf3627b3416
Cheesky <ruby.akbar.firmansyah@gmail.com>
2025-07-16 21:42:50 +0700

    MySQL Config dimasukkan

--- src/tubesif7_apkkoperasi/koneksi.java
+++ src/tubesif7_apkkoperasi/koneksi.java
@@ -14,27 +14,23 @@ package tubesif7_apkkoperasi;
  * @author RANI AMALIYAH
  */
 public class koneksi {
-    public Properties mypanel, myLanguange;
+    public Properties mypanel, myLanguage;
     private String strNamePanel;
-    public koneksi()
-    {
+    public koneksi(){
+    
         
     }
-    public String SettingPanel(String nmPanel)
-    {
-        try
-        {
+    public String SettingPanel(String nmPanel){
+    
+        try{
+        
             mypanel = new Properties();
-            mypanel.load(new FileInputStream
-                    ("lib/database.ini"));
+            mypanel.load(getClass().getClassLoader().getResourceAsStream("lib/database.ini"));
             strNamePanel = mypanel.getProperty(nmPanel);
         }
-        catch(Exception e)
-        {
-            JOptionPane.showMessageDialog(null,
-                    e.getMessage(),"Error",
-                    JOptionPane.INFORMATION_MESSAGE
-            );
+        catch(Exception e){
+        
+            JOptionPane.showMessageDialog(null, e.getMessage(),"Error",JOptionPane.INFORMATION_MESSAGE);
             System.err.println(e.getMessage());
             System.exit(0);
         }
